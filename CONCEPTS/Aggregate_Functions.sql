eba2e1fb2c4b75fbc4ba53cfe235dd69ad98b1c1
Rohan Nair <rohannair145@gmail.com>
2026-05-28 23:17:50 +0530

    Add SQL examples for aggregate functions
    Add CONCEPTS/Aggregate_Functions.sql containing example aggregate queries using the orders table: COUNT, SUM, MAX, MIN and AVG. These samples demonstrate basic aggregation and column aliasing for instructional/reference purposes.

---
+++ CONCEPTS/Aggregate_Functions.sql
@@ -0,0 +1,9 @@
+select count(*) as tota_order_nos from orders;
+
+select sum(sales) as tota_sale from orders;
+
+select max(sales) as max_sale from orders;
+
+select min(sales) as min_sale from orders;
+
+select avg(sales) as avg_sale from orders;
